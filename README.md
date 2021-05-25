d4d2317ff866b03b35e5f8960ba1b11e30b6a8da
Jason Don <jdon@etsy.com>
2021-05-24 17:22:02 -0700

    visible spectrum demo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-python3 -m http.server
+# Color
+An interactive about colors
+
+#### Start up a simple server to serve the website
+From the color directory
+```python3 -m http.server```
--- resources/color.js
+++ resources/color.js
@@ -3,8 +3,8 @@ import rough from 'https://unpkg.com/roughjs?module';
 
 // Global Constants
 const WIDTH = 500;
-const HEIGHT = 300;
-const DELAY = 75;
+const HEIGHT = 200;
+const DELAY = 100;
 
 // Global contexts
 let canvas; //canvas object
@@ -12,8 +12,6 @@ let ctx; //canvas context
 let rc; //roughjs canvas object
 
 document.addEventListener("DOMContentLoaded", initCanvas);
-
-// run on DOMContentLoaded
 export function initCanvas() {
 	canvas = document.getElementById('canvas');
 	canvas.width = WIDTH;
@@ -27,8 +25,36 @@ export function initCanvas() {
 
 function draw() {
   clear();
-  let hex = wavelengthToColor(400)[0]
-  rc.rectangle(0,0,50,50, {fill:hex, fillStyle:'solid', roughness:1, seed:10})
+  let wavelength = document.getElementById("wired-slider").value;
+  let rgba = wavelengthToColor(wavelength)[0];
+  let rect_width = 300
+  let rect_height = 100
+
+  rc.rectangle(centerX(rect_width), centerY(rect_height)-40, rect_width,rect_height, {fill:rgba, fillStyle:'solid', roughness:1});
+
+  
+  let gradient_width = 400;
+  let gradient_height = 50;
+  let gradient_x = centerX(gradient_width)
+  let gradient_y = centerY(gradient_height) + 70
+
+  var grd = ctx.createLinearGradient(gradient_x, 0, gradient_x+gradient_width, 0);
+  let stops = 10
+  for (let i = 0; i<=stops; i++) {
+    //linearly map i from 400 to 650
+    let wave = i*(250/stops) + 400
+    grd.addColorStop(i*(1/stops), wavelengthToColor(wave)[0])
+  }
+  rc.rectangle(gradient_x, gradient_y, gradient_width,gradient_height, {fill:grd, fillStyle:'solid', roughness:0});
+
+}
+
+function centerX(object_width) {
+  return WIDTH/2 - object_width/2;
+}
+
+function centerY(object_height) {
+  return HEIGHT/2 - object_height/2;
 }
 
 function clear() {
@@ -39,8 +65,9 @@ function clear() {
 // takes wavelength in nm and returns an rgba value
 // from https://scienceprimer.com/javascript-code-convert-light-wavelength-color
 function wavelengthToColor(wavelength) {
-  let R, G, B, alpha, colorSpace;
-  let gamma = 1;
+  // modified to display full solid color, alpha is constant
+  // works well from 400-650
+  let R, G, B;
 
   if (wavelength >= 380 && wavelength < 440) {
       R = -1 * (wavelength - 440) / (440 - 380);
@@ -72,24 +99,9 @@ function wavelengthToColor(wavelength) {
       B = 0;
   }
 
-  // intensty is lower at the edges of the visible spectrum.
-  if (wavelength > 780 || wavelength < 380) {
-      alpha = 0;
-  } else if (wavelength > 700) {
-      alpha = (780 - wavelength) / (780 - 700);
-  } else if (wavelength < 420) {
-      alpha = (wavelength - 380) / (420 - 380);
-  } else {
-      alpha = 1;
-  }
+  // make alpha a little softer for aesthetics
+  let alpha = 0.75;
 
-  colorSpace = ["rgba(" + (R * 100) + "%," + (G * 100) + "%," + (B * 100) + "%, " + alpha + ")", R, G, B, alpha]
-
-  // colorSpace is an array with 5 elements.
-  // The first element is the complete code as a string.  
-  // Use colorSpace[0] as is to display the desired color.  
-  // use the last four elements alone or together to access each of the individual r, g, b and a channels.  
-  
+  let colorSpace = ["rgba(" + (R * 100) + "%," + (G * 100) + "%," + (B * 100) + "%, " + alpha + ")", R, G, B, alpha]
   return colorSpace;
-    
 }
